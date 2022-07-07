a068f5d23fcfe027d720fee242ff9aca872fc407
shimada337 <eisukeqoo@icloud.com>
2022-07-07 17:38:56 +0900

    [start]php

---
+++ lesson01/sample01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>sample01</title>
+</head>
+<body>
+  <?php
+  echo 'PHPテスト';
+  echo 'ああああ';
+  echo "I'm aa";
+  echo 'I\'m aa';
+  ?>
+</body>
+</html>
