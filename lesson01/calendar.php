1d6581828d165b25149f7929e46a67936a471ee7
shimada337 <eisukeqoo@icloud.com>
2022-07-09 23:36:03 +0900

    ][end]カレンダー

--- lesson01/calendar.php
+++ lesson01/calendar.php
@@ -1,7 +1,8 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
-$time = strtotime('+1 day'); //unixタイムスタンプ
-echo $time . '<br>';
-$day = date('n/j(D)', $time);
-echo $day . '<br>';
+for ($i=0; $i<366; $i++):
+  $time = strtotime("+$i day"); //ダブルクォーテーションで変数の内容を展開している
+  $day = date('n/j(D)', $time);
+  echo $day . '<br>';
+endfor;
 ?>
